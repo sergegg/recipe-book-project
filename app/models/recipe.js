748e593b93f68e9a05019585f8954127205b8b0f
sergey honiyan <sergehoniyan@gmail.com>
2021-01-06 11:44:29 -0500

    Commit new Schema
    Added recipe.js Schema which includes all the fields i'd like my V1
    project to have users add.

---
+++ app/models/recipe.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  ingredients: {
+    type: String,
+    required: true
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: URL,
+    required: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Recipe', recipeSchema)
